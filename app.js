731942ef7d253228b4b33adcec2b3d80f36765ef
mateocanov21hyphen <mateo.cano.vanegas.2005@gmail.com>
2023-11-13 01:53:31 -0500

    Nuestro primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log('Petición recibida')
+
+    res.send('<h1>Hola Mundo con NODEMODE!</h1>')
+})
+
+app.listen(4000, () => {
+    console.log('Servidor escuchando en el puerto 4000')
+})
+
+
+
