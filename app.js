be1bc64b20cc42581f2df301a8341ceaf233f81f
mateocanov21hyphen <mateo.cano.vanegas.2005@gmail.com>
2023-11-13 03:07:58 -0500

    cambios

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     console.log('Petición recibida')
 
 
-    res.send('<h1>Hola Mundo sin .env</h1>')
+    res.status(200).send('<h1>Hola Mundo sin .env</h1>')
 })
 
 const PORT = process.env.PORT
