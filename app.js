67687b48b268251591247a9ae29cff0c4bddca0f
mateocanov21hyphen <mateo.cano.vanegas.2005@gmail.com>
2023-11-13 15:31:20 -0500

    Conexión con HTML, CSS, JS y MONGODB

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
 require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log('Petición recibida')
+mongoose.connect(`mongodb+srv://mateocanovanegas2005:${process.env.MONGO_DB_PASS}@development.oj7ijc7.mongodb.net/MM-app?retryWrites=true&w=majority`)
+    .then((result) => {
+        app.listen(PORT, () => {
+            console.log(`Servido escuchando en el puerto ${PORT}`)
+        })
+        console.log('Conexión exitosa a la BD')
+    })
+    .catch((err) => console.log(err))
 
-    res.status(200).sendFile("index.html", {root: __dirname})
+const productShema = mongoose.Schema(
+    {
+        name: { type: String, require: true },
+        price: Number,
+    },
+    { timestamps: true}
+)
+
+const Product = mongoose.model('Product', productShema)
+
+app.use(express.json())
+
+app.post('/api/v1/products', (req, res) => {
+
+    const newProduct = new Product(req.body)
+
+    newProduct
+    .save()
+    .then( result  => {
+        res.status(201).json({ ok: true })
+    })
+    .catch((err) => console.log(err))
 })
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT
-
-app.listen(parseInt(PORT), () => {
-    console.log('Servidor escuchando en el puerto', PORT)
-})
-
-
-
-
-
